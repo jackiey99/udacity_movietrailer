4cc96eda3292ba2316a383e14c5dbba5fe8463c3
Liangyue Li <jackiey99@gmail.com>
2016-03-25 17:51:46 -0700

    correct warnings in JS Hint

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -20,33 +20,40 @@ main_page_head = '''
         body {
             padding-top: 80px;
         }
+        
         #trailer .modal-dialog {
             margin-top: 200px;
             width: 640px;
             height: 480px;
         }
+        
         .hanging-close {
             position: absolute;
             top: -12px;
             right: -12px;
             z-index: 9001;
         }
+        
         #trailer-video {
             width: 100%;
             height: 100%;
         }
+        
         .movie-tile {
             margin-bottom: 20px;
             padding-top: 20px;
         }
+        
         .movie-tile:hover {
             background-color: #EEE;
             cursor: pointer;
         }
+        
         .scale-media {
             padding-bottom: 56.25%;
             position: relative;
         }
+        
         .scale-media iframe {
             border: none;
             height: 100%;
@@ -59,27 +66,27 @@ main_page_head = '''
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function(event) {
             // Remove the src so the player itself gets removed, as this is the only
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
         // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+        $(document).on('click', '.movie-tile', function(event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id');
             var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
             $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
+                'id': 'trailer-video',
+                'type': 'text-html',
+                'src': sourceUrl,
+                'frameborder': 0
             }));
         });
         // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
+        $(document).ready(function() {
+            $('.movie-tile').hide().first().show("fast", function showNext() {
+                $(this).next("div").show("fast", showNext);
+            });
         });
     </script>
 </head>
